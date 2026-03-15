ca204432030011e1b3292736e290e9a2ed86b7b7
thewages75-crypto <thewages75@gmail.com>
2026-03-15 23:26:34 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+apt-get update
+apt-get install -y libglib2.0-0 libnss3 libatk1.0-0 libatk-bridge2.0-0 libcups2 libxkbcommon0 libxcomposite1 libxdamage1 libxfixes3 libxrandr2 libgbm1 libpango-1.0-0 libpangocairo-1.0-0 libasound2 libatspi2.0-0 libgtk-3-0
+
+pip install -r requirements.txt
+
+playwright install chromium
+
+python insta.py
